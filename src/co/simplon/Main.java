0951c0e61b6793fd56e7869117d79e8454b6252d
Utilisateur <stephane.rodolausse.pf1@gmail.com>
2018-04-02 13:39:43 +0200

    Last commit with boucle

--- src/co/simplon/Main.java
+++ src/co/simplon/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main { 
+public class Main {
 
 	private static Scanner in = new Scanner(System.in);
 
@@ -34,7 +34,7 @@ public class Main {
 				break;
 
 			case "2":
-
+				displayCharSuit();
 				break;
 
 			case "0":
@@ -44,9 +44,7 @@ public class Main {
 
 			default:
 				System.out.println("Donnez une réponse correcte");
-
 			}
-
 		} while (!endOfProg);
 
 		in.close();
@@ -57,7 +55,7 @@ public class Main {
 	// mode console. A chaque question le joueur peut gagner 1 point s'il répond
 	// correctement. S'il répond faux, donner lui la bonne réponse pour qu'il ne se
 	// trompe pas la prochaine fois !
-	
+
 	public static void playingQuizz() {
 		int score = 0;
 		Quizz question1 = new Quizz("Quelle est la couleur du ciel ?", "Bleu", 1);
@@ -73,63 +71,73 @@ public class Main {
 		boolean correctAnswer2 = false;
 		boolean correctAnswer3 = false;
 		do {
-		while (!correctAnswer1) {
-			System.out.println("Voici la première question");
-			System.out.println(question1.getQuestion());
-			String answer1 = in.nextLine();
-			if (answer1.equalsIgnoreCase(question1.getAnswer())) {
-				score += question1.getPoints();
-				correctAnswer1 = true;
-				System.out.println("Bravo, vous marquez 1 point");
-				System.out.println("Votre score est de " + score);
-			} else {
-				System.out.println("Mauvaise réponse. Il fallait répondre : " + question1.getAnswer());
-				System.out.println("Vous pourrez retenter votre chance à la fin du quizz.");
+			while (!correctAnswer1) {
+				System.out.println("Voici la première question");
+				System.out.println(question1.getQuestion());
+				String answer1 = in.nextLine();
+				if (answer1.equalsIgnoreCase(question1.getAnswer())) {
+					score += question1.getPoints();
+					correctAnswer1 = true;
+					System.out.println("Bravo, vous marquez 1 point");
+					System.out.println("Votre score est de " + score);
+				} else {
+					System.out.println("Mauvaise réponse. Il fallait répondre : " + question1.getAnswer());
+					System.out.println("Vous pourrez retenter votre chance à la fin du quizz.");
+				}
+
+				while (!correctAnswer2) {
+					System.out.println("Voici la deuxième question");
+					System.out.println(question2.getQuestion());
+					String answer2 = in.nextLine();
+					if (answer2.equalsIgnoreCase(question2.getAnswer())) {
+						score += question2.getPoints();
+						correctAnswer2 = true;
+						System.out.println("Bravo, vous marquez 1 point");
+						System.out.println("Votre score est de " + score);
+					} else {
+						System.out.println("Mauvaise réponse. Il fallait répondre : " + question2.getAnswer());
+						System.out.println("Vous pourrez retenter votre chance à la fin du quizz.");
+					}
+
+					while (!correctAnswer3) {
+						System.out.println("Voici la troisième question");
+						System.out.println(question3.getQuestion());
+						String answer3 = in.nextLine();
+						if (answer3.equalsIgnoreCase(question3.getAnswer())) {
+							score += question3.getPoints();
+							correctAnswer3 = true;
+							System.out.println("Bravo, vous marquez 1 point");
+							System.out.println("Votre score est de " + score);
+						} else {
+							System.out.println("Mauvaise réponse. Il fallait répondre : " + question3.getAnswer());
+							System.out.println("Vous pourrez retenter votre chance à la fin du quizz.");
+						}
+					}
+				}
 			}
-		
-		while (!correctAnswer2) {
-			System.out.println("Voici la deuxième question");
-			System.out.println(question2.getQuestion());
-			String answer2 = in.nextLine();
-			if (answer2.equalsIgnoreCase(question2.getAnswer())) {
-				score += question2.getPoints();
-				correctAnswer2 = true;
-				System.out.println("Bravo, vous marquez 1 point");
-				System.out.println("Votre score est de " + score);
-			} else {
-				System.out.println("Mauvaise réponse. Il fallait répondre : " + question2.getAnswer());
-				System.out.println("Vous pourrez retenter votre chance à la fin du quizz.");
-			}
-		
-		while(!correctAnswer3) {
-			System.out.println("Voici la troisième question");
-			System.out.println(question3.getQuestion());
-			String answer3 = in.nextLine();
-			if (answer3.equalsIgnoreCase(question3.getAnswer())) {
-				score += question3.getPoints();
-				correctAnswer3 = true;
-				System.out.println("Bravo, vous marquez 1 point");
-				System.out.println("Votre score est de " + score); 
-			}
-			else {
-				System.out.println("Mauvaise réponse. Il fallait répondre : " + question3.getAnswer());
-				System.out.println("Vous pourrez retenter votre chance à la fin du quizz.");
-			}
-			}
-		}
-		}
-		}
-		while(score<3);
+		} while (score < 3);
 
 		System.out.println("vous avez gagné !");
 
 	}
 
-
-
 	// Ecrire un algorithme qui demande à l'utilisateur de rentrer un entier qui
 	// définira la longueur d'une chaîne de caractères que vous construirez ensuite
 	// à base de "#" et que vous afficherez ensuite dans la console. Exemple : si
 	// l'utilisateur rentre '6' vous aurez en sortie "######".
-
+	
+	public static void displayCharSuit() {
+		
+		String myChar = "#"; 
+		System.out.println("Choisissez le nombre de charactère '#' à afficher");
+		int nbChar;
+		nbChar = in.nextInt();
+		in.nextLine();
+		String print = "";
+		for(int nb = 0; nb < nbChar; nb++) {
+			 print += myChar;
+		}
+		System.out.println(print);
+	}
+	
 }
